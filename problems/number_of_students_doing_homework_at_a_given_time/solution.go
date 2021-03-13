0a595ed6073f3b91024f0e451e7818be12c86068
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-03-13 15:46:16 +0000

    Sync LeetCode submission - Number of Students Doing Homework at a Given Time (golang)

---
+++ problems/number_of_students_doing_homework_at_a_given_time/solution.go
@@ -0,0 +1,9 @@
+func busyStudent(startTime []int, endTime []int, queryTime int) int {
+	res := 0
+	for i := 0; i < len(startTime); i++ {
+		if startTime[i] <= queryTime && endTime[i] >= queryTime {
+			res++
+		}
+	}
+	return res
+}
