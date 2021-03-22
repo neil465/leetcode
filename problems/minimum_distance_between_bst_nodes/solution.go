392e7f547293f78848100c5c22a60edb521a0c25
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-03-22 17:11:02 +0000

    Sync LeetCode submission - Minimum Distance Between BST Nodes (golang)

---
+++ problems/minimum_distance_between_bst_nodes/solution.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDiffInBST(root *TreeNode) int {
+	stack := []*TreeNode{root}
+	arr := []int{}
+	for len(stack) > 0 {
+		value := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if value != nil {
+			arr = append(arr, value.Val)
+			stack = append(stack, value.Left, value.Right)
+		}
+	}
+
+	sort.Ints(arr)
+	minimum := abs(arr[0], arr[1])
+	for i := 0; i < len(arr)-1; i ++ {
+		if abs(arr[i], arr[i+1])<minimum {
+			minimum = abs(arr[i], arr[i+1])
+		}
+	}
+	return minimum
+}
+func abs(val int, val2 int) int {
+	if val-val2 < 0 {
+		return -(val - val2)
+	}
+	return val - val2
+}
