6c56f45b1d983cae12120761b252f822dd3146d2
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-06-15 00:29:07 +0000

    Sync LeetCode submission - Single Number (golang)

--- problems/single_number/solution.go
+++ problems/single_number/solution.go
@@ -1,19 +1,7 @@
 func singleNumber(nums []int) int {
-	sort.Ints(nums)
-	fmt.Println(nums)
-	if len(nums) == 1 {
-		return nums[0]
-	}
-	if nums[len(nums)-1] != nums[len(nums)-2] {
-		return nums[len(nums)-1]
-	}
-	if nums[0] != nums[1] {
-		return nums[0]
-	}
-	for i := 1; i < len(nums)-1; i++ {
-		if nums[i-1] != nums[i] && nums[i+1] != nums[i] {
-			return nums[i]
-		}
-	}
-	return 0
+    res := 0
+    for _,i := range nums {
+        res ^= i
+    }
+    return res
 }
