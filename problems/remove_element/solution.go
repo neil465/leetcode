18fdafe5cf5b042b5391381e4d6a7b8b46f9c305
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-06-23 21:22:14 +0000

    Sync LeetCode submission - Remove Element (golang)

--- problems/remove_element/solution.go
+++ problems/remove_element/solution.go
@@ -1,28 +1,10 @@
 func removeElement(nums []int, val int) int {
-	sort.Ints(nums)
-	//fmt.Println(nums)
-	flag := true
-	counter := 0
-	n := len(nums)
-	for i := 0; i < n; i++ {
-
-		if nums[counter] == val {
-			flag = false
-			//fmt.Println("foo", nums[counter], counter)
-			if counter == len(nums)-1 {
-				//fmt.Println("inside", nums[counter])
-				nums = nums[:len(nums)-1]
-			} else {
-				nums = append(nums[:counter], nums[counter+1:]...)
-				//fmt.Println("foo2", "nums", nums)
-			}
-
-		}
-		if flag {
-			counter++
-		}
-
-	}
-	//fmt.Println(nums)
-	return len(nums)
+    count := 0
+    for _,i := range nums{
+        if i != val {
+            nums[count] = i
+            count++
+        }
+    }
+    return count
 }
