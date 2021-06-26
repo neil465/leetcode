f105ec0d3471c0cefb2266e441f5d59f1f0469a1
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-06-26 13:56:53 +0000

    Sync LeetCode submission - Shortest Word Distance (golang)

--- problems/shortest_word_distance/solution.go
+++ problems/shortest_word_distance/solution.go
@@ -1,11 +1,19 @@
 func shortestDistance(wordsDict []string, word1 string, word2 string) int {
-    min := 100000000000000
-    for k,i := range wordsDict{
-        if i == word1{
-            for n,j := range wordsDict{
-                if j == word2 && min > abs(n-k){
-                    min = abs(n-k)
-                }
+    min := 999999999909099090
+    w1 := -1
+    w2 := -1
+    
+    for i := 0 ; i < len(wordsDict) ; i++{
+        if wordsDict[i] == word1{
+            w1 = i
+            if w2 != -1 && abs(w2 - w1) < min{
+                min = abs(w2 - w1)
+            }
+        }
+        if wordsDict[i] == word2{
+            w2 = i
+            if w1 != -1 && abs(w1 - w2) < min{
+                min = abs(w1 - w2)
             }
         }
     }
