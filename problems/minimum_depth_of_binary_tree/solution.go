ac22980a89766752f56127ab8ab10fe3c49caa8f
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-07-21 19:48:17 +0000

    Sync LeetCode submission - Minimum Depth of Binary Tree (golang)

--- problems/minimum_depth_of_binary_tree/solution.go
+++ problems/minimum_depth_of_binary_tree/solution.go
@@ -8,37 +8,20 @@
  */
 func minDepth(root *TreeNode) int {
     if root == nil{return 0}
-    level := 1
-    q := []*TreeNode{root}
-    helper(q,&level)
-    return level
-}
-
-func helper (q []*TreeNode , level *int){
-    arr := []*TreeNode{}
-    for len(q) > 0 {
-        pop := q[0]
-        q = q[1:]
-        if pop.Left != nil{
-            arr= append(arr,pop.Left)
+    q,level := []*TreeNode{root},0
+    for len(q) > 0{
+        newQ := []*TreeNode{}
+        for _,i := range q{
+            if i.Left == nil && i.Right == nil {
+               
+                return level+1
+            }
+            if i.Left != nil{newQ = append(newQ,i.Left)}
+            if i.Right != nil{newQ = append(newQ,i.Right)}
             
-            if pop.Left.Right == nil && pop.Left.Left == nil{
-                *level++
-                return
-            }
         }
-        if pop.Right != nil{
-            arr= append(arr,pop.Right)
-            if pop.Right.Left == nil && pop.Right.Right == nil{
-                *level++
-                return
-            }
-        }
-        
+        if len(newQ) > 0{level++}
+        q = newQ
     }
-    if len(arr)> 0{
-        *level++
-        helper(arr,level)
-    }
-    
+    return level
 }
