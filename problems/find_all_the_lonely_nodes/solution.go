506acdc2f47760466277064f9c80a1e285f56b2e
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-07-26 16:09:56 +0000

    Sync LeetCode submission - Find All The Lonely Nodes (golang)

--- problems/find_all_the_lonely_nodes/solution.go
+++ problems/find_all_the_lonely_nodes/solution.go
@@ -6,23 +6,44 @@
  *     Right *TreeNode
  * }
  */
-func getLonelyNodes(root *TreeNode) []int {
-    g := []int{}
-    helper(root,&g)
-    return g
-}
+// func getLonelyNodes(root *TreeNode) []int {
+//     g := []int{}
+//     helper(root,&g)
+//     return g
+// }
 
-func helper(root *TreeNode , g*[]int){
-    if root == nil{return}
-    if root.Left == nil && root.Right != nil{
-        *g = append(*g,root.Right.Val)
+// func helper(root *TreeNode , g*[]int){
+//     if root == nil{return}
+//     if root.Left == nil && root.Right != nil{
+//         *g = append(*g,root.Right.Val)
 
-    }
-    if root.Left != nil && root.Right == nil{
-        *g = append(*g,root.Left.Val)
+//     }
+//     if root.Left != nil && root.Right == nil{
+//         *g = append(*g,root.Left.Val)
 
-    }
-    helper(root.Left,g)
-    helper(root.Right,g)
+//     }
+//     helper(root.Left,g)
+//     helper(root.Right,g)
  
+// }
+
+func getLonelyNodes(root *TreeNode) []int {
+    q,result := []*TreeNode{root},[]int{}
+    for len(q) > 0{
+        pop := q[0]
+        q = q[1:]
+        if pop.Left != nil{
+            q = append(q,pop.Left)
+            if pop.Right == nil{
+                result = append(result , pop.Left.Val)
+            }
+        }
+        if pop.Right != nil{
+            q = append(q,pop.Right)
+            if pop.Left == nil{
+                result = append(result , pop.Right.Val)
+            }
+        }
+    }
+    return result
 }
