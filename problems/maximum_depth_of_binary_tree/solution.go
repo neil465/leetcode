59abd9500da9fde57962a3d1d9921fe10dd94e07
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-07-26 16:27:40 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (golang)

--- problems/maximum_depth_of_binary_tree/solution.go
+++ problems/maximum_depth_of_binary_tree/solution.go
@@ -6,21 +6,31 @@
  *     Right *TreeNode
  * }
  */
-func maxDepth(root *TreeNode) int {
-    if root == nil{
-        return 0
-    }
-    level := 1
-    q := []*TreeNode{root}
-    for len(q) > 0{
-        arr := []*TreeNode{}
-        for _,i := range q{
-            if i.Left != nil {arr = append(arr,i.Left)}
-            if i.Right != nil {arr = append(arr,i.Right)}
-        }
-        if len(arr) > 0{level++}
-        q = arr
-    }
-    return level
+// func maxDepth(root *TreeNode) int {
+//     if root == nil{
+//         return 0
+//     }
+//     level := 1
+//     q := []*TreeNode{root}
+//     for len(q) > 0{
+//         arr := []*TreeNode{}
+//         for _,i := range q{
+//             if i.Left != nil {arr = append(arr,i.Left)}
+//             if i.Right != nil {arr = append(arr,i.Right)}
+//         }
+//         if len(arr) > 0{level++}
+//         q = arr
+//     }
+//     return level
 
+// }
+func maxDepth(root *TreeNode) int {
+    if root == nil {return 0}
+    return 1+max(maxDepth(root.Left),maxDepth(root.Right))
+}
+func max (i,j int)  int{
+    if i > j{
+        return i 
+    }
+    return j
 }
