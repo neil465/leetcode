706c7a0c1334716cccc174098f4badf64e6e8925
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-08-18 18:32:29 +0000

    Sync LeetCode submission - Reverse Linked List (golang)

--- problems/reverse_linked_list/solution.go
+++ problems/reverse_linked_list/solution.go
@@ -6,26 +6,10 @@
  * }
  */
 func reverseList(head *ListNode) *ListNode {
-    curr := head 
-    arr := []int{}
-    for curr != nil{
-        if curr !=nil{
-            arr = append(arr,curr.Val)
-            curr = curr.Next
-        }
-        
-        
-    }
-    if len(arr) == 0{
-        return head
-    }
-    fmt.Println(arr)
-    l := &ListNode{Val:arr[len(arr)-1]}
-    curr2 := l  
-    for i := len(arr)-2 ; i >= 0 ; i --{
-        l1 := &ListNode{Val:arr[i]}
-        curr2.Next = l1
-        curr2 = curr2.Next
-    }
-    return l
+	var prev *ListNode
+    
+	for head != nil {
+		head.Next, prev, head = prev, head, head.Next
+	}
+	return prev
 }
