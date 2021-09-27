5d43ade873d375d0dcb42b152e186a11e59150b4
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-09-27 21:56:16 +0000

    Sync LeetCode submission - The kth Factor of n (golang)

--- problems/the_kth_factor_of_n/solution.go
+++ problems/the_kth_factor_of_n/solution.go
@@ -1,13 +1,9 @@
 func kthFactor(n int, k int) int {
     count := 0
-    for i := 1 ; i <= n ; i++{
-        if n%i == 0{
-            count++
-            if count == k{
-                return i
-            }
-            
-        }
+    for i := 1; i <= n/2 ; i++{
+        if count ==k-1 && n%i == 0{return i}
+        if n%i == 0{count++}
     }
+    if count== k-1 {return n}
     return -1
 }
