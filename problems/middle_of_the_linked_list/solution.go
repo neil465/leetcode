7d4e603f72d56a8bb37fc8657511b569c86b3d54
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-10-04 13:54:55 +0000

    Sync LeetCode submission - Middle of the Linked List (golang)

--- problems/middle_of_the_linked_list/solution.go
+++ problems/middle_of_the_linked_list/solution.go
@@ -5,25 +5,16 @@
  *     Next *ListNode
  * }
  */
-// func middleNode(head *ListNode) *ListNode {
-//     m := make(map[int]*ListNode)
-//     count := 0
-//     for head != nil{
-//         m[count] = head
-//         head = head.Next
-//         count++
-//     }
-//     return m[count/2]
-// }
 func middleNode(head *ListNode) *ListNode {
-    count := 0
-    c := head
-    for c != nil {
-        count++
-        c = c.Next
+    t,h := head,head
+    for h != nil{
+        h = h.Next
+        if h != nil{
+            h = h.Next
+        }else{
+            break
+        }
+        t = t.Next
     }
-    for i := 0 ; i < count/2 ; i++{
-        head = head.Next
-    }
-    return head
+    return t
 }
