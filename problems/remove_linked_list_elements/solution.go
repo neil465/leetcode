6ebd40838693c0c1c7d1344cb4e79a594d13987b
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-11-02 13:32:20 +0000

    Sync LeetCode submission - Remove Linked List Elements (golang)

--- problems/remove_linked_list_elements/solution.go
+++ problems/remove_linked_list_elements/solution.go
@@ -6,27 +6,7 @@
  * }
  */
 func removeElements(head *ListNode, val int) *ListNode {
-    if head == nil{
-        return nil
-    }
-    k := &ListNode{}
-	c := k
-	var prev *ListNode
-    curr := head
-    for curr != nil{
-        if curr.Val != val{
-            c.Val = curr.Val
-		    l1 := &ListNode{}
-		    prev = c
-		    c.Next =l1
-		    c = c.Next
-        }
-        curr = curr.Next
-    }
-    if k.Next!= nil{
-        fmt.Println(k)
-        prev.Next = nil 
-        return k
-    }
-    return nil
+    if head == nil{return nil}
+    head.Next = removeElements(head.Next,val)
+    if head.Val == val{return head.Next}else{return head}
 }
