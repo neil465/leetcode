21df67268ddf3edeade55b19a0d4969453843a9d
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-11-10 14:56:08 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (golang)

---
+++ problems/search_in_rotated_sorted_array_ii/solution.go
@@ -0,0 +1,9 @@
+func search(nums []int, target int) bool {
+    if len(nums) == 1{
+        return nums[0] == target
+    }
+    if search(nums[:len(nums)/2],target) || search(nums[len(nums)/2:],target){
+        return true
+    }
+    return false
+}
