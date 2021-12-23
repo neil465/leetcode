4d22473bb1afb1646887bad425b6d7d7161a3fb4
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2021-12-23 16:11:16 +0000

    Sync LeetCode submission - Convert Binary Number in a Linked List to Integer (golang)

--- problems/convert_binary_number_in_a_linked_list_to_integer/solution.go
+++ problems/convert_binary_number_in_a_linked_list_to_integer/solution.go
@@ -6,17 +6,11 @@
  * }
  */
 func getDecimalValue(head *ListNode) int {
-    res := 0
-    curr := head
-    arr := []int{}
-    for curr != nil{
-        arr = append(arr,curr.Val)
-        curr = curr.Next
+    s := ""
+    for head != nil{
+        s += fmt.Sprint(head.Val)
+        head = head.Next
     }
-    multiplyer := 1
-    for i := len(arr)-1 ; i>=0 ; i--{
-        res += multiplyer*arr[i]
-        multiplyer*=2
-    }    
-    return res
+    a, _ := strconv.ParseInt(s,2,64)
+    return int(a)
 }
