a8d47043c2cd010a21eb3df6ad81348cd3e88e04
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-01-23 16:31:35 +0000

    Sync LeetCode submission - Jump Game (golang)

--- problems/jump_game/solution.go
+++ problems/jump_game/solution.go
@@ -1,16 +1,14 @@
 func canJump(nums []int) bool {
-    i := 0
-    for dist := 0 ; i <= dist ; i++{
-        dist = Max(dist,i+nums[i])
+    i := 0 
+    for dist := 0 ;  i <= dist ; i++{
+        dist = max(dist,i+nums[i])
         if i >= len(nums)-1{
             return true
         }
     }
     return false
 }
-func Max(x, y int) int {
-    if x < y {
-        return y
-    }
-    return x
+func max (i,j int) int{
+    if i > j { return i }
+    return j
 }
