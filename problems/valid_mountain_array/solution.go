7ddcfebcb474d5a4cb3d959eeab669233d952e63
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-01-25 14:51:05 +0000

    Sync LeetCode submission - Valid Mountain Array (golang)

--- problems/valid_mountain_array/solution.go
+++ problems/valid_mountain_array/solution.go
@@ -1,33 +1,14 @@
-import "fmt"
 func validMountainArray(arr []int) bool {
-    flag := true
-    if len(arr) == 1{
-        return false
+    if len(arr) < 3 { return false }
+    
+    left := 0
+    right := len(arr)-1 
+    
+    for left = 0 ; left+1 <len(arr) && arr[left] < arr[left+1];left++ {
+        continue 
     }
-	for i := 0; i < len(arr)-1; i++ {
-		
-		if arr[i] == arr[i+1] {
-			return false
-		}
-		if arr[i] > arr[i+1]{
-			if i==0 {
-				return false
-			}else {
-                fmt.Println("down")
-				flag = false
-                
-			}
-		}
-        fmt.Println(flag)
-		if arr[i] < arr[i+1] && flag == false{
-            
-            fmt.Println("up")
-            return false
-        }
-      
-	}
-    if len(arr)>1 && arr[len(arr)-1]>= arr[len(arr)-2]  {
-		return false
-	}
-	return true
+    for right = len(arr)-1 ; right > 0 && arr[right] < arr[right-1]; right--{
+        continue 
+    }
+    return left == right && left != 0 && right != len(arr) - 1
 }
