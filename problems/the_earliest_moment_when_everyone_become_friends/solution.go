e66a2a0ca924d038f56890d8308043f152b4b444
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-02-01 16:07:01 +0000

    Sync LeetCode submission - The Earliest Moment When Everyone Become Friends (golang)

--- problems/the_earliest_moment_when_everyone_become_friends/solution.go
+++ problems/the_earliest_moment_when_everyone_become_friends/solution.go
@@ -1,42 +1,39 @@
 type unionFind struct {
-	parents []int
 	count   int
+	parents []int
 }
 
 func (this *unionFind) Find(x int) int {
 	if x == this.parents[x] {
 		return x
 	}
-
 	this.parents[x] = this.Find(this.parents[x])
 	return this.parents[x]
 }
 
-func (this *unionFind) Union(x int, y int) {
+func (this *unionFind) Union(x, y int) {
 	rx, ry := this.Find(x), this.Find(y)
-	if rx == ry {
-		return
+	if rx != ry {
+		this.parents[rx] = ry
+		this.count--
 	}
-
-	this.count--
-	this.parents[rx] = ry
 }
+
 
 func earliestAcq(logs [][]int, n int) int {
     sort.Slice(logs,func(i,j int)bool{
         return logs[i][0] < logs[j][0]
     })
-    a := make([]int,n)
-    u := &unionFind{parents:a,count:n}   
+    arr := make([]int,n)
+    union := &unionFind{count : n,parents : arr}
     for i := 0 ; i < n ; i++{
-        u.parents[i] = i
+        union.parents[i] = i
     }
     for _,i := range logs{
-        u.Union(i[1],i[2])
-        if u.count == 1{
+        union.Union(i[1],i[2])
+        if union.count == 1 {
             return i[0]
         }
     }
     return -1
-    
 }
