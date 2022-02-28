da5a36794cc66873c95a933df4c55c679591800c
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-02-28 17:21:56 +0000

    Sync LeetCode submission - House Robber (golang)

--- problems/house_robber/solution.go
+++ problems/house_robber/solution.go
@@ -1,24 +1,15 @@
+var dp [100000]int
+
 func rob(nums []int) int {
-    memo := make([]int,len(nums))
-    for i := 0 ; i < len(memo) ; i++{
-        memo[i] = -1
-    }
-    var robber func([]int,int) int 
-    robber = func(nums[]int,pos int) int {
-        if pos < 0 {
-            return 0
-        }
-        if memo[pos] > -1 {
-            return memo[pos]
-        }
-        memo[pos] = max(robber(nums,pos-2)+nums[pos],robber(nums,pos-1))
-        return memo[pos]
-    }
+    dp = [100000]int{}
     return robber(nums,len(nums)-1)
 }
-func max (i,j int) int {
-    if i > j {
-        return i
+func robber(nums []int, pos int) int {
+    if pos < 0 {
+        return 0
     }
-    return j
+    if dp[pos] <= 0 {
+        dp[pos] = int(math.Max(float64(robber(nums,pos-2)+nums[pos]),float64(robber(nums,pos-1))))+1    
+    }
+    return dp[pos]-1
 }
