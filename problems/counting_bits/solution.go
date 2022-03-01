647983d1991e72eaac32a005aa88e58e02e4b7c1
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-03-01 14:46:58 +0000

    Sync LeetCode submission - Counting Bits (golang)

--- problems/counting_bits/solution.go
+++ problems/counting_bits/solution.go
@@ -1,15 +1,25 @@
-func countBits(num int) []int {
-    result := []int{}
-    for i:=0;i<=num;i++{
-        x := uint32(i)
-        res := 0
-        for x >0 {
-            if x & uint32(1) == 1{
-                res ++
-            }
-            x>>=1
-        }
-        result = append(result,res)
+var dp [100000]int
+
+func countBits(n int) []int {
+    dp = [100000]int{}
+    res := []int{}
+    for i := 0 ; i <= n ; i++{
+        res = append(res,bits(i))
     }
-    return result
+    return res
+}
+func bits(i int) int{
+    if dp[i] > 0 {
+        return dp[i]
+    }
+    if i == 0 {
+        return 0
+    }else if i == 1{
+        return 1
+    }else if i%2 == 0 {
+        dp[i] = bits(i/2) 
+        return dp[i]
+    }
+    dp[i] = 1+bits(i/2)
+    return dp[i]
 }
