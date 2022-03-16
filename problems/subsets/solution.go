2951d92daf9cb5406213e2e3dc31ad2b2b861176
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-03-16 17:23:05 +0000

    Sync LeetCode submission - Subsets (golang)

--- problems/subsets/solution.go
+++ problems/subsets/solution.go
@@ -1,34 +1,15 @@
-// func subsets(nums []int) [][]int {
-//     res := [][]int{}
-//     for i := 0 ; i < int(math.Pow(2,float64(len(nums)))); i++{
-//         num := i
-     
-//         temp := []int{}
-//         pointer := 0
-//         for num != 0{
-//             if num & 1 == 1{
-//                 temp = append(temp,nums[pointer])
-//             }
-//             pointer++
-//             num = num >> 1
-//         }
-//         res = append(res,temp)
-//     }
-//     return res
-// }
+var result [][]int
 
 func subsets(nums []int) [][]int {
-    subsets := [][]int{}
-    backtracking(0,nums,[]int{},&subsets)
-    return subsets
+    result = [][]int{}
+    backtrack(nums, []int{})
+    return result
 }
-func backtracking(index int , nums []int, subs []int, subsets *[][]int){
-    b := make([]int, len(subs))
-    copy(b, subs)
-    *subsets = append(*subsets,b)
-    for i := index ; i < len(nums) ; i++{
-        subs = append(subs,nums[i])
-        backtracking(i+1,nums,subs,subsets)
-        subs = subs[:len(subs)-1]
+func backtrack(nums []int, res []int) {
+    
+    result = append(result, append([]int{}, res...))
+    
+    for i := range nums{
+        backtrack(nums[i + 1:], append(res, nums[i]))
     }
 }
