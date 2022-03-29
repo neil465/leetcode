b4dbc10f3cc53475a521e8cb152eed34ca23dabc
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-03-29 15:00:00 +0000

    Sync LeetCode submission - Find the Duplicate Number (golang)

--- problems/find_the_duplicate_number/solution.go
+++ problems/find_the_duplicate_number/solution.go
@@ -1,10 +1,9 @@
-import "sort"
 func findDuplicate(nums []int) int {
-	sort.Ints(nums)
-	for i := 1; i < len(nums); i++ {
-		if nums[i-1] == nums[i]  {
-			return nums[i]
-		}
-	}
-	return 0
+    sort.Ints(nums) 
+    for i := 0 ; i < len(nums) - 1 ; i ++{
+        if nums[i] == nums[i+1] {
+            return nums[i]
+        }
+    }
+    return -1
 }
