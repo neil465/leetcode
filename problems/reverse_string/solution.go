c1ac8bfbe44685f0a0d9eac63e3447287e5e3230
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-04-01 15:29:32 +0000

    Sync LeetCode submission - Reverse String (golang)

--- problems/reverse_string/solution.go
+++ problems/reverse_string/solution.go
@@ -1,8 +1,5 @@
 func reverseString(s []byte)  {
-    helper(0,len(s)-1,&s) 
-}
-func helper (i int ,j int ,s *[]byte){
-	if i >= j{return}
-	(*s)[i], (*s)[j] = (*s)[j],(*s)[i]
-	helper(i+1,j-1,s)
+    for i, j := 0, len(s) - 1 ; i <= j; i,j = i + 1 , j - 1 {
+        s[i], s[j] = s[j], s[i]
+    } 
 }
