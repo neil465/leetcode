fa12b49c6e598f06235618f325f4d2f9e2b31562
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-04-07 14:30:09 +0000

    Sync LeetCode submission - Last Stone Weight (golang)

--- problems/last_stone_weight/solution.go
+++ problems/last_stone_weight/solution.go
@@ -1,23 +1,38 @@
 func lastStoneWeight(stones []int) int {
+    h := &IntHeap{}
+	heap.Init(h)
     
-    for len(stones) >1 {
-        sort.Ints(stones)
-        if stones[0] == stones[1] && len(stones) == 2{
-            return 0
-        }
-        if stones[len(stones)-1] == stones[len(stones)-2]{
-            fmt.Println(stones)
-            stones = stones[:len(stones)-1]
-            fmt.Println(stones)
-            stones = stones[:len(stones)-1]
-            fmt.Println(stones)
-            
-        }else{
-            stones[len(stones)-1] = stones[len(stones)-1] - stones[len(stones)-2]
-            stones = append(stones[:len(stones)-2], stones[len(stones)-1:]...)
-        }
-        
-        
+    for _, i := range stones {
+        heap.Push(h, i)
     }
-    return stones[0]
+    
+    for h.Len() > 1 {
+        a := heap.Pop(h).(int) - heap.Pop(h).(int)
+        if a > 0 {
+            heap.Push(h, a)
+        }
+    }
+    if h.Len() == 0 {
+        return 0
+    }
+    return heap.Pop(h).(int)
+}
+
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
