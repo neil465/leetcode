16e79234232d2eed7bb2fda701b1ef7e0d1500fa
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-06-20 15:59:26 +0000

    Sync LeetCode submission - Container With Most Water (golang)

--- problems/container_with_most_water/solution.go
+++ problems/container_with_most_water/solution.go
@@ -1,15 +1,16 @@
+
 func maxArea(height []int) int {
     max := 0 
-    for i, j := 0, len(height) - 1; i <= j; i = i {
-        max = compare(max, compare(height[i], height[j], false) * (j - i), true)
-        if height[i] < height[j] {
-            i++
-        }else{
-            j--
+    l, r := 0, len(height) - 1
+    for l <= r {
+        if max < int(math.Min(float64(height[l]), float64(height[r]))) * (r - l) {
+            max = int(math.Min(float64(height[l]), float64(height[r]))) * (r - l)
+        }
+        if height[l] < height[r] {
+            l++
+        } else {
+            r --
         }
     }
     return max
-}
-func compare(i, j int, b bool ) int {
-    if b { if i > j { return i }; return j } else { if i > j { return j } else { return i } }
 }
