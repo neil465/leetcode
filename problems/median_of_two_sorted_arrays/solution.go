665b735a706bbe520abb2e3733176d5105629ad6
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-07-19 14:42:02 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (golang)

--- problems/median_of_two_sorted_arrays/solution.go
+++ problems/median_of_two_sorted_arrays/solution.go
@@ -1,8 +1,9 @@
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-    nums1 = append(nums1,nums2...)
+    nums1 = append(nums1, nums2...)
     sort.Ints(nums1)
-    if len(nums1)%2 != 0{
+    if len(nums1) %2!=0{
         return float64(nums1[len(nums1)/2])
+    }else {
+        return float64(nums1[len(nums1)/2] + nums1[(len(nums1)/2)-1])/float64(2)
     }
-    return ( float64(nums1[len(nums1)/2]) + float64(nums1[(len(nums1)/2)-1]))/2
 }
