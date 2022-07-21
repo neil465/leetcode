175d6c52fe12b48cddb91582dc7f3ea0dac58108
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-07-21 14:59:45 +0000

    Sync LeetCode submission - Add Digits (golang)

--- problems/add_digits/solution.go
+++ problems/add_digits/solution.go
@@ -1,30 +1,9 @@
 func addDigits(num int) int {
-	if num < 10 {
-		return num
-	}
-	res := 0
-
-	var x []int
-	for num > 0 {
-		x = append(x, num%10)
-		num = num / 10
-	}
-	//fmt.Println("array", x)
-	for _, i2 := range x {
-		res += i2
-
-	}
-	//fmt.Println("result", res)
-
-	if res > 9 {
-		//fmt.Println("inside the lowest if", res)
-		return addDigits(res)
-
-	} else {
-		//fmt.Println("resultfromelse", res)
-		return res
-
-	}
-	return res
-
+    if num < 10 { return num }
+    sum := 0
+    for num > 0 {
+        sum += num%10
+        num /= 10
+    }
+    return addDigits(sum)
 }
