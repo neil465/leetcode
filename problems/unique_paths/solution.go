84a1ccf8523dcb3b8c3262316414237ad6ba0c66
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-08-01 15:19:34 +0000

    Sync LeetCode submission - Unique Paths (golang)

--- problems/unique_paths/solution.go
+++ problems/unique_paths/solution.go
@@ -1,14 +1,16 @@
+var dp = map[[2]int]int{}
+
 func uniquePaths(m int, n int) int {
-    return pathFinder(m,n, map[string]int{})
+    dp = map[[2]int]int{}
+    return pathFinder(m,n)
 }
-func pathFinder(m int, n int, dp map[string]int) int {
-    var pos = fmt.Sprint(m) + "," + fmt.Sprint(n)
-    
-    if (m == 1 || n == 1 ) || dp[pos] > 0 {
-        return dp[pos] + 1 
+func pathFinder(m int, n int) int {
+    if dp[[2]int{m,n}] > 0 {
+        return dp[[2]int{m,n}]
     }
-    
-    dp[pos] = pathFinder(m - 1, n, dp) + pathFinder(m, n - 1, dp) - 1
-    
-    return dp[pos] + 1
+    if m == 1 && n == 1{return 1}
+    if m == 1{return 1}
+    if n == 1{return 1}
+    dp[[2]int{m,n}] = pathFinder(m-1,n)+pathFinder(m,n-1)
+    return dp[[2]int{m,n}]
 }
