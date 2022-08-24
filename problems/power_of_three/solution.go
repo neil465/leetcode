61e0790b68f13b345bf74119fc62670803affecf
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-08-24 16:39:30 +0000

    Sync LeetCode submission - Power of Three (golang)

--- problems/power_of_three/solution.go
+++ problems/power_of_three/solution.go
@@ -1,9 +1,7 @@
 func isPowerOfThree(n int) bool {
-	if n < 1 {
-		return false
-	}
-	for n%3 ==0  {
-		n/=3
-	}
-	return n==1
+    sum := 1 
+    for sum < n {
+        sum *= 3
+    }
+    return sum == n
 }
