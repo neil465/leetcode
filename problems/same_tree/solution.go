d8731697208ffd001333aa271be5f68ea39eacba
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2022-09-14 17:04:37 +0000

    Sync LeetCode submission - Same Tree (golang)

--- problems/same_tree/solution.go
+++ problems/same_tree/solution.go
@@ -1,35 +1,20 @@
-func isSameTree(r1 *TreeNode, r2 *TreeNode) bool {
-    if r1 != nil && r2 != nil && r1.Val != r2.Val{return false}
-    if r1 == nil && r2 == nil{return true}
-    if (r1 == nil && r2 != nil)||(r1!=nil && r2==nil){return false}
-	q := []*TreeNode{r1}
-    q2 := []*TreeNode{r2}
-    g,g2 := []int{},[]int{}
-    helper(q,&g)
-    helper(q2,&g2)
-    if reflect.DeepEqual(g,g2){
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil && q == nil {
         return true
     }
-	return false
-	
-}
-func helper(q []*TreeNode , g *[]int){
-    if len(q)  == 0{return}
-    pop := q[0]
-    q = q[1:]
-    if pop.Left != nil{
-        *g = append(*g,pop.Left.Val)
-        q = append(q,pop.Left)
+    if p == nil && q != nil || p != nil && q == nil{
+        return false
     }
-    if pop.Left == nil{
-        *g = append(*g,-100002)
-    }
-    if pop.Right != nil{
-        *g = append(*g,pop.Right.Val)
-        q = append(q,pop.Right)
-    }
-    if pop.Right == nil{
-        *g = append(*g,-100002)
-    }
-    helper(q,g)
+    if p.Val != q.Val {
+        return false
+    } 
+    return isSameTree(p.Left,q.Left) && isSameTree(p.Right, q.Right)
 }
