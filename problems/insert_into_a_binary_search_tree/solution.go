59d1d4dcc073e6ff25e3e9305e72c5d1dad87245
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2023-06-30 13:18:36 +0000

    Sync LeetCode submission - Insert into a Binary Search Tree (golang)

---
+++ problems/insert_into_a_binary_search_tree/solution.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+    if root == nil {
+        return &TreeNode{Val:val}
+    }
+    if root.Val > val {
+        if root.Left == nil{
+            root.Left = &TreeNode{Val: val}
+            return root
+        }
+        
+        insertIntoBST(root.Left, val)
+    } else if root.Val < val {
+
+        if root.Right == nil{
+            root.Right = &TreeNode{Val: val}
+            return root
+        }
+        
+        insertIntoBST(root.Right, val)
+    }
+    
+    return root
+}
