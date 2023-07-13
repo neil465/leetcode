c90c4675d58280302a3d6ad1649dd0f76e6b1415
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2023-07-13 13:29:42 +0000

    Sync LeetCode submission - Divide Array Into Equal Pairs (golang)

---
+++ problems/divide_array_into_equal_pairs/solution.go
@@ -0,0 +1,9 @@
+func divideArray(nums []int) bool {
+    sort.Ints(nums)
+    for i := 0 ; i < len(nums); i += 2 {
+        if nums[i] != nums[i + 1] {
+            return false
+        }
+    } 
+    return true
+}
