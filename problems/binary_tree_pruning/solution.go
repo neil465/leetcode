57baffb84252194ac162d861a704e0f30a4bded3
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2023-07-17 13:34:02 +0000

    Sync LeetCode submission - Binary Tree Pruning (golang)

---
+++ problems/binary_tree_pruning/solution.go
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pruneTree(root *TreeNode) *TreeNode {
+    if root == nil { return nil }
+    root.Left, root.Right =  pruneTree(root.Left), pruneTree(root.Right)
+    if root.Val == 0 && root.Left == nil && root.Right == nil { return nil }
+    return root
+}
