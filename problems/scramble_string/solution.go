f029baea45f6ad0f5fac7531e602ac07c61b2332
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2023-10-19 17:05:14 +0000

    Sync LeetCode submission - Scramble String (golang)

---
+++ problems/scramble_string/solution.go
@@ -0,0 +1,24 @@
+var dp = map[[2]string]bool{}
+
+func isScramble(s1 string, s2 string) bool {
+    dp = map[[2]string]bool{}
+    return re(s1, s2)
+}
+func re(s1, s2 string) bool {
+    if _, ok := dp[[2]string{s1,s2}]; ok {
+
+        return dp[[2]string{s1,s2}]
+    }
+    if len(s1) == 1 {
+        return s1 == s2
+    }
+    v := false
+    for i := 1; i < len(s1) ; i++ {
+        if (re(s1[:i], s2[:i]) && re(s1[i:], s2[i:])) || (re(s1[i:], s2[:len(s2) - i]) && re(s1[:i], s2[len(s2) - i :])) {
+            v = true
+            break
+        }
+    }
+    dp[[2]string{s1,s2}] = v
+    return v
+}
