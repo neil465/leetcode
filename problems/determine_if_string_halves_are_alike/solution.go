8de592788622383de61be6a80adecfb78b3559e2
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2024-01-12 15:05:32 +0000

    Sync LeetCode submission - Determine if String Halves Are Alike (golang)

--- problems/determine_if_string_halves_are_alike/solution.go
+++ problems/determine_if_string_halves_are_alike/solution.go
@@ -1,18 +1,24 @@
+
+
 func halvesAreAlike(s string) bool {
-	vouls := []string{"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
-	sum := 0
-	sum2 := 0
-	firstHalf := s[:len(s)/2]
-	lastHalf := s[len(s)/2:]
-	for i, i2 := range firstHalf {
-		for _, voul := range vouls {
-			if voul == string(i2) {
-				sum++
-			}
-			if voul == string(lastHalf[i]) {
-				sum2++
-			}
-		}
-	}
-	return sum == sum2
+    pre1 := []int{}
+    curCount := 0
+
+    for i := 0 ; i < len(s); i ++ {
+        if s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' {
+            curCount ++
+        }
+        pre1 = append(pre1, curCount)
+    }
+    pre2 := []int{}
+    curCount = 0
+
+    for i := len(s) - 1 ; i >= 0; i -- {
+        if s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' {
+            curCount ++
+        }
+        pre2 = append([]int{curCount}, pre2...)
+    }
+    fmt.Println(pre1, pre2)
+    return pre1[len(s)/2 - 1] == pre2[len(s)/2 ] 
 }
