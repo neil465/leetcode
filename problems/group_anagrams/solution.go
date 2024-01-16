03953295fb5e716900786a131b3e71197616ea75
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2024-01-16 17:36:10 +0000

    Sync LeetCode submission - Group Anagrams (golang)

--- problems/group_anagrams/solution.go
+++ problems/group_anagrams/solution.go
@@ -1,33 +1,18 @@
 func groupAnagrams(strs []string) [][]string {
-    m := map[string][]string{}
-    
-    for _,i := range strs{
-        m[SortString(i)] = append(m[SortString(i)],i)
+    m := map[[26]int][]string{}
+
+    for _, i := range strs {
+        a := [26]int{}
+
+        for _, j := range i {
+            a[int(j) - int('a')]++
+        }
+        m[a] = append(m[a], i)
     }
-    result := [][]string{}
-    
-    for _,i := range m {
-        result = append(result,i)
+    k := [][]string{}
+
+    for _, v := range m {
+        k = append(k, v)
     }
-    return result
-}
-
-type sortRunes []rune
-
-func (s sortRunes) Less(i, j int) bool {
-    return s[i] < s[j]
-}
-
-func (s sortRunes) Swap(i, j int) {
-    s[i], s[j] = s[j], s[i]
-}
-
-func (s sortRunes) Len() int {
-    return len(s)
-}
-
-func SortString(s string) string {
-    r := []rune(s)
-    sort.Sort(sortRunes(r))
-    return string(r)
+    return k
 }
