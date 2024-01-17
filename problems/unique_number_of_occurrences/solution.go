4dc5560dd51703f275da92fc77a3353d0cc095a5
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2024-01-17 16:35:21 +0000

    Sync LeetCode submission - Unique Number of Occurrences (golang)

--- problems/unique_number_of_occurrences/solution.go
+++ problems/unique_number_of_occurrences/solution.go
@@ -1,19 +1,14 @@
 func uniqueOccurrences(arr []int) bool {
-	mappy := make(map[int]int)
-	arr2 := []int{}
-	for _, i2 := range arr {
-		mappy[i2]++
-	}
-	for _, i2 := range mappy {
-        
-		for i := 0; i < len(arr2); i++ {
-			if i2 == arr2[i] {
-				return false
-			}
-            
-		}
-        arr2 = append(arr2, i2)
-			
-	}
-	return true
+    valMap, freqMap := map[int]int{}, map[int]bool{} 
+
+    for _, i := range arr {
+        valMap[i] ++
+    }
+    for _, v := range valMap {
+        if freqMap[v]{
+            return false
+        }
+        freqMap[v] = true
+    }
+    return true
 }
