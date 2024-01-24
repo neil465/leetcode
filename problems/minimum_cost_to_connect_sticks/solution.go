faa42b0d0d074db5dd78ab23f01a8e75e6d50b06
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2024-01-24 14:58:40 +0000

    Sync LeetCode submission - Minimum Cost to Connect Sticks (golang)

--- problems/minimum_cost_to_connect_sticks/solution.go
+++ problems/minimum_cost_to_connect_sticks/solution.go
@@ -1,31 +1,40 @@
-import ("container/heap")
-func connectSticks(sticks []int) int {
-    m := &IntHeap{}
-    heap.Init(m)
-    sum := 0
-    for _,i := range sticks{
-        heap.Push(m,i)
-    }
-    for m.Len() > 1{
 
-        a := heap.Pop(m)
-        b := heap.Pop(m)
-        sum += a.(int)+b.(int)
-        heap.Push(m,a.(int)+b.(int))
-   
-    }
-    return sum
-}
-
+import "container/heap"
 type IntHeap []int
 
 func (h IntHeap) Len() int           { return len(h) }
 func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
 func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *IntHeap) Pop() interface{} {
-    old := *h
-    pop := old[len(old)-1]
-    *h = old[:len(old)-1]
-    return pop
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
 }
-func (h *IntHeap) Push(x interface{}) {*h = append(*h, x.(int))}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func connectSticks(sticks []int) int {
+    
+    h := &IntHeap{}
+
+    for _, i := range sticks {
+        heap.Push(h, i)
+    }
+    sum := 0
+    for h.Len() > 1 {
+        v1 := heap.Pop(h).(int)
+
+        v2 := heap.Pop(h).(int)
+        sum += v1 + v2
+        heap.Push(h, v1 + v2)
+
+    }
+
+    return sum
+
+}
