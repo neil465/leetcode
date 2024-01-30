dc3f2cb537754d69f04f1d2ba0ea8b11277238a7
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2024-01-30 16:37:02 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (golang)

--- problems/evaluate_reverse_polish_notation/solution.go
+++ problems/evaluate_reverse_polish_notation/solution.go
@@ -1,21 +1,28 @@
+
+import "strconv"
 func evalRPN(tokens []string) int {
     s := []int{}
-    for _, token := range tokens {
-        if token != "+" && token != "-" && token != "*" && token != "/" {
-            intToken, _ := strconv.Atoi(token)
-            s = append(s, intToken)
-        }else{
-            a , b := s[len(s) - 2],s[len(s) - 1]
+
+    for i := range tokens {
+        if tokens[i] == "+" {
+            v := s[len(s) - 1] + s[len(s) - 2]
             s = s[:len(s) - 2]
-            if token == "+" {
-                s = append(s, a + b)
-            }else if token == "/"{
-                s = append(s, a / b)
-            }else if token == "*"{
-                s = append(s, a * b)
-            }else if token == "-"{
-                s = append(s, int(float64(a) - float64(b)))
-            }
+            s = append(s, v)
+        } else if  tokens[i] == "*" {
+            v := s[len(s) - 1] * s[len(s) - 2]
+            s = s[:len(s) - 2]
+            s = append(s, v)
+        } else if tokens[i] == "/" {
+            v := s[len(s) - 2] / s[len(s) - 1]
+            s = s[:len(s) - 2]
+            s = append(s, v)
+        } else if tokens[i] == "-" {
+            v := s[len(s) - 2] - s[len(s) - 1]
+            s = s[:len(s) - 2]
+            s = append(s, v)
+        } else {
+            v, _ := strconv.Atoi(tokens[i])
+            s = append(s, v)
         }
     }
     return s[0]
