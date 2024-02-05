54b292de57efb2ae8e4728c679b1962184993de5
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2024-02-05 15:31:00 +0000

    Sync LeetCode submission - Minimum Time to Revert Word to Initial State I (golang)

---
+++ problems/minimum_time_to_revert_word_to_initial_state_i/solution.go
@@ -0,0 +1,10 @@
+
+import "strings"
+func minimumTimeToInitialState(word string, k int) int {
+    for i := k ; i < len(word) ; i += k {
+        if strings.HasPrefix(word, word[i:]) {
+            return i / k
+        }
+    }
+    return int(math.Ceil(float64(len(word))/ float64(k)))
+}
