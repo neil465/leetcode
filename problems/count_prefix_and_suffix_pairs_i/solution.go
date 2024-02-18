e9f356e03239fa71211079dc460cdb218facc363
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2024-02-18 02:31:26 +0000

    Sync LeetCode submission - Count Prefix and Suffix Pairs I (golang)

---
+++ problems/count_prefix_and_suffix_pairs_i/solution.go
@@ -0,0 +1,12 @@
+func countPrefixSuffixPairs(words []string) int {
+    k := 0
+    
+    for i := range words {
+        for j := i + 1 ; j < len(words) ; j ++ {
+            if strings.HasPrefix(words[j], words[i]) && strings.HasSuffix(words[j], words[i]) {
+                k ++
+            }
+        }
+    }
+    return k
+}
