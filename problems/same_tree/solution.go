17fc7d1b3123908ed0e32bc1c8245116fd8ae48a
neilnaveen <42328488+neilnaveen@users.noreply.github.com>
2024-02-26 15:57:48 +0000

    Sync LeetCode submission - Same Tree (golang)

--- problems/same_tree/solution.go
+++ problems/same_tree/solution.go
@@ -7,14 +7,11 @@
  * }
  */
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-    if p == nil && q == nil {
-        return true
-    }
-    if p == nil && q != nil || p != nil && q == nil{
-        return false
+    if (p == nil || q == nil) {
+        return p == nil && q == nil
     }
     if p.Val != q.Val {
         return false
-    } 
-    return isSameTree(p.Left,q.Left) && isSameTree(p.Right, q.Right)
+    }
+    return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right) 
 }
